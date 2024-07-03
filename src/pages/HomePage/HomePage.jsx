cd4aa66755e2416522194c86c7ef48c129995dcc
Holovko Maksym <golovkomaksim852@gmail.com>
2024-07-03 14:00:58 +0300

    bg/fix

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,22 @@
+import { useSelector } from "react-redux";
 import Creators from "../../components/Creators/Creators";
 import Logo from "../../components/Logo/Logo";
 import css from "./HomePage.module.css";
+import { selectUser } from "../../redux/auth/selectors";
 
 export default function Home() {
+  const user = useSelector(selectUser);
+
   return (
     <div className={css.global}>
       <Logo />
       <div className={css.container}>
-        <h1 className={css.title}>Welcome!</h1>
-        <p className={css.desc}>It is secure storage of your contacts</p>
+        <h1 className={css.title}>Welcome {user.name}!</h1>
+        {user.name ? (
+          <p className={css.desc}>Here you can add and find your contacts</p>
+        ) : (
+          <p className={css.desc}>It is secure storage of your contacts</p>
+        )}
       </div>
       <Creators />
     </div>
