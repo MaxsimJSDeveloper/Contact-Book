be29f61fa2ab79560689a719feb84c5573384d6e
Holovko Maksym <golovkomaksim852@gmail.com>
2025-09-18 22:25:00 +0300

    update-libs

--- src/components/Creators/Creators.jsx
+++ src/components/Creators/Creators.jsx
@@ -4,8 +4,26 @@ const Creators = () => {
   return (
     <div className={css.creators}>
       <h3 className={css.title}>Creators</h3>
-      <p className={css.text}>Developer: Holovko Maksym</p>
-      <p className={css.text}>Designer: Pavlova Maria</p>
+      <div className={css.box}>
+        <p className={css.text}>Developer:</p>
+        <a
+          className={css.link}
+          href="https://www.linkedin.com/in/maksymholovko/"
+          target="_blank"
+        >
+          Holovko Maksym
+        </a>
+      </div>
+      <div className={css.box}>
+        <p className={css.text}>Designer:</p>
+        <a
+          className={css.link}
+          href="https://www.linkedin.com/in/maria-pavlova-03811b384/"
+          target="_blank"
+        >
+          Pavlova Maria
+        </a>
+      </div>
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter
+          future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+        >
           <App />
         </BrowserRouter>
       </PersistGate>
